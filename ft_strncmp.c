b0c45a26609f47e2f3a70477c14ca5a6e3df05a5
narnault <95000633+narnault@users.noreply.github.com>
2022-12-17 17:08:36 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,36 +1,24 @@
 #include <stdio.h>
 
-int     ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-        unsigned int     i;
-        unsigned int     j;
-        unsigned int     k;
+	int i;
 
-        i = 0;
-        j = 0;
-        k = 0;
-        while (s1[i] != '\0' && i <= n) {
-                j += s1[i];
-                i++;
-        }
-        i = 0;
-        while (s2[i] != '\0' && i <= n) {
-                k += s2[i];
-                i++;
-        }
-        if (j < k)
-          return (0);
-        if (j > k)
-          return (1);
-        if (j == k)
-          return (NULL);
+	i = 0;
+	if (n == 0)
+	    return (0);
+	while ((s1[i] == s2[i]) && (i <= n))
+		i++;
+	return (s1[i] - s2[i]);
 }
 
 int main()
 {
-        unsigned int n = 7;
-        char    *s1 = "qqqqqqqqqqqqq";
-        char    *s2 = "tqqqqqqqqqqqq";
+    unsigned int n = -1;
+    char    *s1 = "salut ca va";
+    char    *s2 = "oui ca va";
+    
+    printf("%i", ft_strncmp(s1, s2, n));
 
-        printf ("%i", ft_strncmp(s1, s2, n));
+    return 0;
 }
